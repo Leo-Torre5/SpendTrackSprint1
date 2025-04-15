7fa7065232d835b0bb7cc53ec14eafdfd2fa7cc3
Félix SCHERER <felix.scherer@laposte.net>
2025-04-15 13:02:23 -0500

    fix: authentication tokens headers not working

--- TrackSpendProject/settings.py
+++ TrackSpendProject/settings.py
@@ -139,14 +139,15 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
 
 SIMPLE_JWT = {
-    'AUTH_HEADER_TYPES': ('JWT',),
+    'AUTH_HEADER_TYPES': ('Bearer',),
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
 }
 
